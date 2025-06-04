d8398eca120b73b8daf884f30d948d98ef7341e4
henning-asmalsky <henning.asmalsky@web.de>
2025-06-04 08:34:42 +0200

    Meine Änderungen: XML Layout hinzugefügt traurig aber

---
+++ app/src/main/kotlin/Frontend/MainActivity.kts
@@ -0,0 +1,16 @@
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> true
+                R.id.nav_info -> true
+                R.id.nav_user -> true
+                else -> false
+            }
+        }
+    }
+}
