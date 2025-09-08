cd2cf9ee51f5bb15d7a9b15e0babfe2e7d4029cc
Martin <43502717+MartinK28@users.noreply.github.com>
2025-09-08 21:50:58 +0200

    AppId vergeben

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.num8rix"
+        applicationId = "com.mind9.app"
         minSdk = 29
         targetSdk = 35
         versionCode = 1
