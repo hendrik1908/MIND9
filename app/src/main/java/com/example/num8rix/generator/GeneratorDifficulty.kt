9b2b62197ed1295bd0cdeb45aad9984a22b36705
Marvin Exel <marv.exel@gmail.com>
2025-09-12 19:25:35 +0200

    Anpassung der schwarzen Felder

--- app/src/main/java/com/example/num8rix/generator/GeneratorDifficulty.kt
+++ app/src/main/java/com/example/num8rix/generator/GeneratorDifficulty.kt
@@ -7,8 +7,8 @@ enum class GeneratorDifficulty(
     val minWhiteClues: Int,
     val description: String
 ) {
-    EASY(4..12, true, 0.4, 20, "Große Kompartments, viele Hinweise"),
-    MEDIUM(13..25, true, 0.3, 15, "Mittlere Kompartments, moderate Hinweise"),
+    EASY(22..34, true, 0.4, 20, "Große Kompartments, viele Hinweise"),
+    MEDIUM(14..25, true, 0.3, 15, "Mittlere Kompartments, moderate Hinweise"),
     HARD(26..38, true, 0.2, 10, "Kleine Kompartments, wenige Hinweise"),
-    EXPERT(10..22, false, 0.1, 8, "Sehr kleine Kompartments, minimale Hinweise")
+    EXPERT(5..18, false, 0.1, 8, "Sehr kleine Kompartments, minimale Hinweise")
 }
