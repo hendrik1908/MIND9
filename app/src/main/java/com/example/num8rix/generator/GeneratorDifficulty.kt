e43a1d0ca654578e34ed5966be69f6892125a6ae
Marvin Exel <marv.exel@gmail.com>
2025-09-13 07:28:34 +0200

    Anpassungen

--- app/src/main/java/com/example/num8rix/generator/GeneratorDifficulty.kt
+++ app/src/main/java/com/example/num8rix/generator/GeneratorDifficulty.kt
@@ -10,5 +10,5 @@ enum class GeneratorDifficulty(
     EASY(22..34, true, 0.4, 20, "Große Kompartments, viele Hinweise"),
     MEDIUM(14..25, true, 0.3, 15, "Mittlere Kompartments, moderate Hinweise"),
     HARD(26..38, true, 0.2, 10, "Kleine Kompartments, wenige Hinweise"),
-    EXPERT(5..18, false, 0.1, 8, "Sehr kleine Kompartments, minimale Hinweise")
+    EXPERT(5..15, false, 0.1, 8, "Sehr kleine Kompartments, minimale Hinweise")
 }
